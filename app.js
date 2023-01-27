3fdbd3a96618eed8e5dd6bbdedcb196ca8d0bf05
Josue Hernandez <josuehdz0@gmail.com>
2023-01-27 11:45:02 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+
+let upgrades =[
+  {
+    name: 'Small Pod',
+    price: 50,
+    value:1,
+  },
+  {
+    name: 'Bigboy UFO',
+    price: 200,
+    value:10,
+  },
+  {
+    name: 'Shuttle',
+    price: 1000,
+    value:10,
+  },
+  {
+    name: "Starfleet",
+    price: 5000,
+    value:1000,
+  },
+  
+  
+]
+
+let humans=0
+
+
+
+function drawClickUpgrade(){
+
+
+
+}
+
+function drawAutoClick(){
+
+}
+
+function abduct(){
+  console.log('test');
+  
+}
+
